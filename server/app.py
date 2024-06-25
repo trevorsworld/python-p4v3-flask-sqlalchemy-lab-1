150bedf6645c4bf02465e3b365a1ca806e69f1e8
trevorsworld <trevors.karithi@student.moringaschool.com>
2024-06-25 21:07:19 +0300

    Completed Assignments

--- server/app.py
+++ server/app.py
@@ -22,6 +22,29 @@ def index():
 
 # Add views here
 
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    quake = Earthquake.query.filter(Earthquake.id==id).first()
+    
+    if quake:
+        body = quake.to_dict()
+        status = 200
+    else:
+        body = {'message':f'Earthquake {id} not found.'}
+        status = 404
+        
+        return make_response(body, status)
+    
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def magnitude_match(magnitude):
+    quakes = []
+    for quake in Earthquake.query.filter(Earthquake.magnitude>=magnitude).all():
+        quakes.append(quake.to_dict())
+        
+        body = {'count': len(quakes),
+                'quakes': quakes
+                }
+        return make_response(body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
